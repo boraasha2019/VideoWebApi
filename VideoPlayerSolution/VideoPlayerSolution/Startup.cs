32bccfabb1e2d9f2124cc437c985359be80e416a
boraasha2019 <48927005+boraasha2019@users.noreply.github.com>
2019-05-12 15:25:27 +0530

    movie playing from wwroot

--- VideoPlayerSolution/VideoPlayerSolution/Startup.cs
+++ VideoPlayerSolution/VideoPlayerSolution/Startup.cs
@@ -53,9 +53,9 @@ namespace VideoPlayerSolution
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseCors("CorsPolicy");
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
